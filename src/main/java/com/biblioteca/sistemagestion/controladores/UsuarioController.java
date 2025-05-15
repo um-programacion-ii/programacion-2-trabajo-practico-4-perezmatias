f82066682cff9810def4840310c21a92869736a7
perezmatias <maag.perez@alumno.um.edu.ar>
2025-05-15 17:18:08 -0300

    feat: Implementar endpoint GET /api/usuarios para obtener todos los usuarios (Etapa 3)

--- src/main/java/com/biblioteca/sistemagestion/controladores/UsuarioController.java
+++ src/main/java/com/biblioteca/sistemagestion/controladores/UsuarioController.java
@@ -4,6 +4,9 @@ import com.biblioteca.sistemagestion.servicios.UsuarioService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.Objects;
+import com.biblioteca.sistemagestion.modelo.Usuario;
+import org.springframework.web.bind.annotation.GetMapping;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/usuarios")
@@ -13,6 +16,11 @@ public class UsuarioController {
 
     public UsuarioController(UsuarioService usuarioService) {
         this.usuarioService = Objects.requireNonNull(usuarioService, "UsuarioService no puede ser nulo.");
+    }
+
+    @GetMapping
+    public List<Usuario> obtenerTodosLosUsuarios() {
+        return usuarioService.obtenerTodosLosUsuarios();
     }
 
 }
