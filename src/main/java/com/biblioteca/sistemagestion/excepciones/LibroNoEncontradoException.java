91d7495963e9f29da38545f4d8c1df95487ac288
perezmatias <maag.perez@alumno.um.edu.ar>
2025-05-14 21:46:54 -0300

    feat: Definir UsuarioNoEncontradoException e interfaz UsuarioService (Etapa 2)

--- src/main/java/com/biblioteca/sistemagestion/excepciones/LibroNoEncontradoException.java
+++ src/main/java/com/biblioteca/sistemagestion/excepciones/LibroNoEncontradoException.java
@@ -1,4 +1,13 @@
 package com.biblioteca.sistemagestion.excepciones;
 
-public class LibroNoEncontradoException {
+public class LibroNoEncontradoException extends RuntimeException {
+
+    public LibroNoEncontradoException(String message) {
+        super(message);
+    }
+
+    public LibroNoEncontradoException(Long id) {
+        super("Libro no encontrado con ID: " + id);
+    }
+
 }
